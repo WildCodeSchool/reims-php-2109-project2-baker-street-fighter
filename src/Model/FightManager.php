0c3da228c2bcd7bcc9a3fb81c45ad8ecded62721
Robin <jonval.robin@gmail.com>
2021-10-27 10:33:27 +0200

    create method for insert fight

--- src/Model/FightManager.php
+++ src/Model/FightManager.php
@@ -6,4 +6,16 @@ class FightManager extends AbstractManager
 {
     public const TABLE = 'fight';
 
+        /**
+     * Insert new item in database
+     */
+    public function insert(array $fight): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`date`, `winner`) VALUES (:date, :winner)");
+        $statement->bindValue('date', $fight['date'], \PDO::PARAM_STR);
+        $statement->bindValue('winner', $fight['winner'], \PDO::PARAM_STR);
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
 }
