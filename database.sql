5310e60f20fc99e9bb60c8cdb70dc72d968b1f78
Antoine Leclabart <antoine.leclabart@gmail.com>
2021-10-27 11:05:18 +0200

    fixing database

--- database.sql
+++ database.sql
@@ -36,7 +36,7 @@ CREATE TABLE fighter (
 CREATE TABLE fight(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     date TIMESTAMP,
-    winner INT NOT NULL,
+    winner VARCHAR(80) NOT NULL,
     PRIMARY KEY (id)
 );
 
