300fd9e81c2dcca4e984a8c36d63292ec4e58cb5
N26N26N26 <jordan.verreaux@gmail.com>
2021-10-27 11:57:54 +0200

    added controller lists fighters

--- src/Controller/FightersController.php
+++ src/Controller/FightersController.php
@@ -6,5 +6,11 @@ use App\Model\FighterManager;
 
 class FightersController extends AbstractController
 {
+    public function index(): string
+    {
+        $listFighters = new FighterManager();
+        $listFighters = $listFighters->selectAll('name');
 
+        return $this->twig->render('Fighters/fighterslist.html.twig', ['listFighters' => $listFighters]);
+    }
 }
