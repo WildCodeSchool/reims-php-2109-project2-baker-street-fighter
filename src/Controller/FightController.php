9c99bffb6a1d1b9ae971cbd677764d4ef8ee34a0
Antoine Leclabart <antoine.leclabart@gmail.com>
2021-10-28 11:54:12 +0200

    initialising fighter 1 and 2

--- src/Controller/FightController.php
+++ src/Controller/FightController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Model\FighterManager;
 use App\Model\FightManager;
+use App\Entity\Fighter;
 
-public function index()
+class FightController extends AbstractController
 {
     public function index(): string
     {
@@ -12,5 +14,39 @@ public function index()
         $fights = $fightManager->selectAll('date');
 
         return $this->twig->render('fight/index.html.twig', ['fights' => $fights]);
+    }
+
+    public function fight(): string
+    {
+        $fighterManager = new FighterManager();
+
+        $fighter1AsArray = $fighterManager->selectOneById(1);
+        var_dump($fighter1AsArray);
+        $fighter1 = null;
+        $fighter2AsArray = $fighterManager->selectOneById(2);
+        var_dump($fighter2AsArray);
+        $fighter2 = null;
+
+        $fighter1 = new Fighter($fighter1AsArray['name'], $fighter1AsArray['attack'], $fighter1AsArray['defense'], $fighter1AsArray['image']);
+        var_dump($fighter1);
+
+        $fighter2 = new Fighter($fighter2AsArray['name'], $fighter2AsArray['attack'], $fighter2AsArray['defense'], $fighter2AsArray['image']);
+        var_dump($fighter2);
+
+        // while ($fighter1->isAlive() && $fighter2->isAlive()) {
+        //     $fighter1->fight($fighter2);
+        //     $fighter2->fight($fighter1);
+        // }
+
+        // if ($fighter1->isAlive()) {
+        //     $winner = $fighter1;
+        //     $loser = $fighter2;
+        // } else {
+        //     $winner = $fighter2;
+        //     $loser = $fighter1;
+        // }
+
+        return $this->twig->render('fight/fight.html.twig', ['winner' => $winner, 'loser' => $loser]);
+        return "let's fight";
     }
 }
