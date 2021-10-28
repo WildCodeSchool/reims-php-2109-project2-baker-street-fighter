947d8144cb4f00e713e0ebb908c1463642cfbde3
Antoine Leclabart <antoine.leclabart@gmail.com>
2021-10-28 11:55:06 +0200

    fixing namespace path

--- src/Model/FightManager.php
+++ src/Model/FightManager.php
@@ -2,26 +2,9 @@
 
 namespace App\Model;
 
-use App\Entities\Fighter;
+use App\Entity\Fighter;
 
-class FightManager extends Fighter
+class FightManager extends AbstractManager
 {
-    public function roundA(){
-        return $playerOne->fightRound($playerTwo);
-    }
-
-    public function roundB(){
-        return $playerTwo->fightRound($playerOne);
-    }
-
-    public function isWinner(){
-        $winner = $this->playerOne->getName();
-        if($this->playerTwo->getLifeValue() > $this->playerOne->getLifeValue()) 
-        {
-        $winner = $this->playerTwo->getName();
-        }
-        return $winner;
-    }
-    
     public const TABLE = 'fight';
 }
