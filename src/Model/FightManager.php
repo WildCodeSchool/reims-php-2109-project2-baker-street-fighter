2993d2f541e24bd9b8e7464c8ecb04ff3383a4ea
N26N26N26 <jordan.verreaux@gmail.com>
2021-10-28 12:29:20 +0200

    added method for selectAll join table 'fights' and 'fighter'

--- src/Model/FightManager.php
+++ src/Model/FightManager.php
@@ -5,6 +5,7 @@ namespace App\Model;
 class FightManager extends AbstractManager
 {
     public const TABLE = 'fight';
+    public const JOINTABLE = 'fighter';
 
     public function insert(array $fight): int
     {
@@ -14,5 +15,18 @@ class FightManager extends AbstractManager
 
         $statement->execute();
         return (int)$this->pdo->lastInsertId();
+    }
+
+    public function selectAll(string $orderBy = '', string $direction = 'ASC'): array
+    {
+        $query =
+        'SELECT * FROM ' . static::TABLE .
+        ' JOIN ' . static::JOINTABLE . ' ON ' . static::JOINTABLE .
+        '.id=' . static::TABLE . ".id;";
+        if ($orderBy) {
+            $query .= ' ORDER BY ' . $orderBy . ' ' . $direction;
+        }
+
+        return $this->pdo->query($query)->fetchAll();
     }
 }
