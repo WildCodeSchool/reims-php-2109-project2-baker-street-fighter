75c96019d520c1d78d6b7d75cf8fe07f5b9557fd
Robin <jonval.robin@gmail.com>
2021-11-09 13:41:29 +0100

    corrected position of init [currentAttacker] & deleted obsolete code

--- src/Controller/FightersController.php
+++ src/Controller/FightersController.php
@@ -31,20 +31,10 @@ class FightersController extends AbstractController
                 header('Location: /fight/pick');
             } elseif (!isset($_SESSION['player2'])) {
                 $_SESSION['player2'] = $fighterManager->selectOneById($_POST['selected_fighter']);
+                $_SESSION['currentAttacker'] = $_SESSION['player1'];
                 header('Location: /fight/attack');
             }
         }
-
-        // initiate fighters
-
-        $player1 = $_SESSION['player1'] ?? null;
-        $player2 = $_SESSION['player2'] ?? null;
-
-        $_SESSION['player1'] = $player1;
-        $_SESSION['player2'] = $player2;
-        $currentAttacker = $player1;
-        $_SESSION['currentAttacker'] = $currentAttacker;
-
         return $this->twig->render('fight/pickFighter.html.twig', ['fighters' => $fighters]);
     }
 }
